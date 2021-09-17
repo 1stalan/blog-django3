6595be8b34b2e74f1fb03f24d3335641297f023c
Alan Santos <alan.profissional@outlook.com>
2021-09-17 16:54:40 -0300

    Configurando django admin

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.db import models
+from django.db.models.base import Model
 from .models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sub_title', 'full_name']
+    search_fields = ['title', 'sub_title', 'full_name']
+    
+
+
+admin.site.register(Post, PostAdmin)
