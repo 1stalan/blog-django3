3b807ecd2ce6c32a7bffb6d364f2e928c9bad532
Alan Santos <alan.profissional@outlook.com>
2021-09-20 11:54:52 -0300

    Get query set novo recurso da versão 3

--- blog/website/models.py
+++ blog/website/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+class Categories(models.TextChoices):
+    tech = 'TC', 'Tecnologia'
+    universe = 'CR', 'Curiosidades'
+    gr = 'GR', 'Geral'
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
+
+    categories = models.CharField(
+        max_length=2,
+        choices=Categories.choices,
+        default=Categories.gr,
+    )
+
+    deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
